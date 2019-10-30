6def91e56b6b17c5f4b50fe4a5e58838e51469f5
Abhishek Sharma <46181804+sharmaji27@users.noreply.github.com>
2019-10-30 12:31:39 +0530

    Update 4. Median of Two Sorted Arrays.py

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -13,12 +13,11 @@ nums2 = [3, 4]
 The median is (2 + 3)/2 = 2.5
 '''
 
-class Solution(object):
-    class Solution:
-        def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-            num3 = sorted(nums1 + nums2)
-            l = len(num3)
-            if l % 2 == 0:
-                return (num3[(l // 2) - 1] + num3[(l // 2)]) / 2
-            else:
-                return num3[l // 2]
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        num3 = sorted(nums1 + nums2)
+        l = len(num3)
+        if l % 2 == 0:
+            return (num3[(l // 2) - 1] + num3[(l // 2)]) / 2
+        else:
+            return num3[l // 2]

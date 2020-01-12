edf7ee01362a0aaebc727a5cf627ae6a51e34557
unknown <asharma70420@gmail.com>
2020-01-12 13:12:31 +0530

    Q27

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -16,6 +16,7 @@ Example 2:
 Given nums = [0,1,2,2,3,0,4,2], val = 2,
 Your function should return length = 5, with the first five elements of nums containing 
 0, 1, 3, 0, and 4.
+
 Note that the order of those five elements can be arbitrary.
 It doesn't matter what values are set beyond the returned length.
 

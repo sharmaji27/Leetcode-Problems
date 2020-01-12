75f24c42fb0ad188b4f53b706210164aaa5160d7
unknown <asharma70420@gmail.com>
2020-01-12 13:10:45 +0530

    Q4

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -28,8 +28,10 @@ input array will be known to the caller as well.
 Internally you can think of this:
 // nums is passed in by reference. (i.e., without making a copy)
 int len = removeElement(nums, val);
+
 // any modification to nums in your function would be known by the caller.
 // using the length returned by your function, it prints the first len elements.
+
 for (int i = 0; i < len; i++) {
     print(nums[i]);
 }

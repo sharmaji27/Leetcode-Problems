b7c13ce3e35762fbf788daeb53adb4dfc9b4a616
unknown <asharma70420@gmail.com>
2020-01-12 13:06:17 +0530

    Q41

--- 41. First Missing Positive.py
+++ 41. First Missing Positive.py
@@ -9,6 +9,13 @@ Example 2:
 Input: [3,4,-1,1]
 Output: 2
 
+Example 3:
+Input: [7,8,9,11,12]
+Output: 1
+
+Note:
+Your algorithm should run in O(n) time and uses constant extra space.
+
 '''
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:

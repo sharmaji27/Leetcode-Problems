4f4e21324ec01465b329b2601e2fee0c4ac2b1a5
unknown <asharma70420@gmail.com>
2020-01-13 18:41:36 +0530

    Q28

--- 28. Implement strStr().py
+++ 28. Implement strStr().py
@@ -1,11 +1,10 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle == '':
-            return 0
         l = len(needle)
+        if l==0:
+            return 0
         for i in range(len(haystack)):
-            if haystack[i]==needle[0]:
-                if haystack[i:i+l]==needle:
-                    return i
-                
+            if haystack[i:i+l]==needle:
+                return i
+
         return -1

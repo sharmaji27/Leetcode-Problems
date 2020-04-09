e8b6e068e4125ca4a055fed1733385e068ebb9e1
unknown <asharma70420@gmail.com>
2020-04-09 19:23:08 +0530

    Q16

--- 16. 3Sum Closest.py
+++ 16. 3Sum Closest.py
@@ -5,6 +5,7 @@ You may assume that each input would have exactly one solution.
 Example:
 Given array nums = [-1, 2, 1, -4], and target = 1.
 The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
 '''
 
 

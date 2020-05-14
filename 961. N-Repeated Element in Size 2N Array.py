d98a491c0d73357ebc6ce525d13c401d97c140e3
sharmaji <asharma70420@gmail.com>
2020-05-14 14:05:30 +0530

    Q961

--- 961. N-Repeated Element in Size 2N Array.py
+++ 961. N-Repeated Element in Size 2N Array.py
@@ -1,5 +1,32 @@
-nums = [1,2,3,4]
-l=[]
-for i in range(0,len(nums),2):
-    l+=([nums[i+1]]*nums[i])
-print(l)
+'''
+In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+
+Return the element repeated N times.
+
+ 
+
+Example 1:
+
+Input: [1,2,3,3]
+Output: 3
+Example 2:
+
+Input: [2,1,2,5,3,2]
+Output: 2
+Example 3:
+
+Input: [5,1,5,2,5,3,5,4]
+Output: 5
+ 
+
+Note:
+
+4 <= A.length <= 10000
+0 <= A[i] < 10000
+A.length is even
+'''
+class Solution:
+    def repeatedNTimes(self, A: List[int]) -> int:
+        for x in A: 
+            if A.count(x)>1: 
+                return x

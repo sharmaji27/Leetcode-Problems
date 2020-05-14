1e3961b1e5b658aa2fd4c277e6f478b98c31be7a
sharmaji <asharma70420@gmail.com>
2020-05-14 11:26:40 +0530

    Q125

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,24 @@
+'''
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+'''
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        p=''
+        for i in s:
+            if i.isalnum():
+                if i.isalpha:
+                    p+=i.lower()
+                else:
+                    p+=i
+        return (p==p[::-1])

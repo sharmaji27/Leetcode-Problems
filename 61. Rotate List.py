2293d57921aa502b916761a6a3bcf6bdff89f865
sharmaji <asharma70420@gmail.com>
2020-05-29 13:33:25 +0530

    Q61

--- 61. Rotate List.py
+++ 61. Rotate List.py
@@ -16,7 +16,7 @@ Explanation:
 rotate 1 steps to the right: 2->0->1->NULL
 rotate 2 steps to the right: 1->2->0->NULL
 rotate 3 steps to the right: 0->1->2->NULL
-rotate 4 steps to the right: 2->0->1->NULL
+rotate 4 steps to the right: 2->0->1->NULL 
 '''
 # Definition for singly-linked list.
 # class ListNode:

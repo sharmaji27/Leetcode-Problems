7d289768ff3c6479984af89f60c9ade6c37c2c04
sharmaji <asharma70420@gmail.com>
2020-06-21 17:40:58 +0530

    Q328

--- 328. Odd Even Linked List.py
+++ 328. Odd Even Linked List.py
@@ -44,3 +44,27 @@ class Solution:
             jumps+=1
             left = left.next
         return head
+
+########################### EFFICIENT SOLUTION ###################################
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:return head
+        odd = head
+        even = head.next
+        eHead = even
+        
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        
+        odd.next = eHead
+        
+        return head

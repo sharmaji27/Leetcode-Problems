4fe67dccd06bed55549adb91ffc21f71bbfd0794
sharmaji <asharma70420@gmail.com>
2020-07-24 12:19:24 +0530

    Q116

---
+++ 116. Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,65 @@
+'''
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Follow up:
+
+You may only use constant extra space.
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+ 
+
+Example 1:
+
+
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+ 
+
+Constraints:
+
+The number of nodes in the given tree is less than 4096.
+-1000 <= node.val <= 1000
+'''
+# Definition for a  binary tree node
+# class TreeLinkNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+
+class Solution:
+    # @param root, a tree node
+    # @return nothing
+    def connect(self, root):
+        if root==None:
+            return
+        q = [root]
+        while q:
+            temp=[]
+            level = []
+            while q:
+                curr = q.pop(0)
+                level.append(curr)
+                if curr.left:temp.append(curr.left)
+                if curr.right:temp.append(curr.right)
+            q=temp
+            if len(level)==1:
+                level[0].next=None
+            else:
+                for i in range(len(level)-1):
+                    level[i].next=level[i+1]
+        return root
